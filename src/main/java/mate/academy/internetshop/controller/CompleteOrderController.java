1cdb7d8d64683d0cab7219c2b76918cbfa54773c
Joanna <i1johanna1s@gmail.com>
2019-09-23 13:04:55 +0300

    Add blank line at the end of files

--- src/main/java/mate/academy/internetshop/controller/CompleteOrderController.java
+++ src/main/java/mate/academy/internetshop/controller/CompleteOrderController.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class CheckoutController extends HttpServlet {
+public class CompleteOrderController extends HttpServlet {
     @Inject
     private static BucketService bucketService;
     @Inject
