103648ebce4b1c092af2f91c144badbc11cc7166
Joanna <i1johanna1s@gmail.com>
2019-09-26 14:56:14 +0300

    Refactor ItemDaoJdbcImpl and IndexController

--- src/main/java/mate/academy/internetshop/controller/IndexController.java
+++ src/main/java/mate/academy/internetshop/controller/IndexController.java
@@ -3,6 +3,7 @@ package mate.academy.internetshop.controller;
 import mate.academy.internetshop.lib.Inject;
 import mate.academy.internetshop.model.Item;
 import mate.academy.internetshop.service.ItemService;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -18,10 +19,14 @@ public class IndexController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        itemService.create(new Item("zebra", 45.));
-        itemService.get(42L);
-        itemService.update(new Item(42L, "zebra", 96.));
-        itemService.delete(42L);
+        Item item = itemService.create(new Item("zebra", 45.));
+        Item itemFromDB = itemService.get(item.getId());
+        Logger logger = Logger.getLogger(IndexController.class);
+        logger.debug("Item from DB = " + item.toString());
+        itemFromDB.setPrice(111000.);
+        Item updatedItem = itemService.update(itemFromDB);
+        logger.debug("Updated item = " + updatedItem.toString());
+        itemService.delete(updatedItem.getId());
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
--- src/main/java/mate/academy/internetshop/dao/jdbc/ItemDaoJdbcImpl.java
+++ src/main/java/mate/academy/internetshop/dao/jdbc/ItemDaoJdbcImpl.java
@@ -6,6 +6,7 @@ import mate.academy.internetshop.model.Item;
 import org.apache.log4j.Logger;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -22,23 +23,20 @@ public class ItemDaoJdbcImpl extends AbstractDao<Item> implements ItemDao {
 
     @Override
     public Item create(Item item) {
-        Statement stmt = null;
-        String query = String.format("INSERT INTO %s.items (name, price) VALUES ('%s', %d)",
-                DB_NAME, item.getName(), item.getPrice().intValue());
-        try {
-            stmt = connection.createStatement();
-            stmt.executeUpdate(query);
-            return item;
-        } catch (SQLException e) {
-            logger.warn("Can't create item = " + item.getName());
-        } finally {
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    logger.warn("Can't close statement", e);
-                }
+        String query = "INSERT INTO " + DB_NAME + ".items (name, price) VALUES (?, ?);";
+        try (PreparedStatement preparedStatement = connection.
+                prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setDouble(2, item.getPrice());
+            preparedStatement.executeUpdate();
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            Item newItem = null;
+            if (rs.next()) {
+                newItem = get(rs.getLong(1));
             }
+            return newItem;
+        } catch (SQLException e) {
+            logger.error("Can't add new item", e);
         }
         return null;
     }
@@ -54,9 +52,7 @@ public class ItemDaoJdbcImpl extends AbstractDao<Item> implements ItemDao {
                 long item_id = rs.getLong("item_id");
                 String name = rs.getString("name");
                 double price = rs.getDouble("price");
-                Item item = new Item(item_id);
-                item.setName(name);
-                item.setPrice(price);
+                Item item = new Item(item_id, name, price);
                 return item;
             }
         } catch (SQLException e) {
@@ -75,23 +71,15 @@ public class ItemDaoJdbcImpl extends AbstractDao<Item> implements ItemDao {
 
     @Override
     public Item update(Item item) {
-        Statement stmt = null;
-        String query = String.format("UPDATE %s.items SET name='%s', price=%d where item_id=%d",
-                DB_NAME, item.getName(), item.getPrice().intValue(), item.getId());
-        try {
-            stmt = connection.createStatement();
-            stmt.executeUpdate(query);
+        String query = "UPDATE " + DB_NAME + ".items SET name= '?', price = '?' WHERE item_id = '?';";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setDouble(2, item.getPrice());
+            preparedStatement.setLong(3, item.getId());
+            preparedStatement.executeUpdate();
             return item;
         } catch (SQLException e) {
-            logger.warn("Can't updete item " + item.getName());
-        } finally {
-            if (stmt != null) {
-                try {
-                    stmt.close();
-                } catch (SQLException e) {
-                    logger.warn("Can't close statement", e);
-                }
-            }
+            logger.error("Can't add new item", e);
         }
         return null;
     }
